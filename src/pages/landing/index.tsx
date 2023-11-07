adc81f921339868491fab25c2371d931e9584357
Atanas Vasilev <my.atanas.vasilev@gmail.com>
2023-11-07 20:52:11 +0200

    Add manual expense input fields

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,22 +1,84 @@
-import { Button, Card, CardBody } from "@nextui-org/react";
+import { Button, Card, CardBody, Input } from "@nextui-org/react";
+import { useState } from "react";
 
 const KingsLanding = () => {
+  const [isBodyExpanded, setIsBodyExpanded] = useState<boolean>(false);
   return (
     <div>
       <Card
         isBlurred
-        className="bg-background/60 dark:bg-default-100/50 h-[120px] w-[100%]"
-        shadow="sm"
+        className="mb-[15px] w-[100%] bg-background/60 dark:bg-default-100/50"
       >
         <CardBody
           style={{
-            display: "flex",
+            display: isBodyExpanded ? "box" : "flex",
+            height: isBodyExpanded ? "220px" : "120px",
             alignItems: "center",
             justifyContent: "center",
           }}
         >
-          <Button className="h-[60px] w-[30%]">Add new expense</Button>
+          {isBodyExpanded ? (
+            <div className="flex w-[90%] flex-col gap-[10px]">
+              <div className="flex flex-1 gap-[10px]">
+                <Input
+                  size="lg"
+                  type="email"
+                  label="Title"
+                  labelPlacement={"inside"}
+                />
+                <Input
+                  size="lg"
+                  type="number"
+                  label="Amount"
+                  placeholder="0.00"
+                  labelPlacement={"inside"}
+                  endContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400">$</span>
+                    </div>
+                  }
+                />
+              </div>
+              <div className="flex w-[50%] flex-1">
+                <Input
+                  size="lg"
+                  type="email"
+                  label="Date"
+                  labelPlacement={"inside"}
+                />
+              </div>
+              <div className="flex w-[100%] flex-1 justify-end gap-[10px]">
+                <Button
+                  onClick={() => {
+                    return;
+                  }}
+                >
+                  Add expense
+                </Button>
+                <Button onClick={() => setIsBodyExpanded(false)}>
+                  Close drawer
+                </Button>
+              </div>
+            </div>
+          ) : (
+            <Button
+              className="h-[60px] w-[30%]"
+              onClick={() => setIsBodyExpanded(true)}
+            >
+              Add new expense
+            </Button>
+          )}
         </CardBody>
+      </Card>
+      <Card
+        isBlurred
+        className="w-[100%] bg-background/60 dark:bg-default-100/50"
+      >
+        <CardBody
+          style={{
+            height: "120px",
+          }}
+        ></CardBody>
       </Card>
     </div>
   );
